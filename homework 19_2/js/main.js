d11c349b8446414868d25ec643c5fdd0222826f0
snitolya <92542355+snitolya@users.noreply.github.com>
2022-03-17 18:44:13 +0300

    Update main.js

--- homework 19_2/js/main.js
+++ homework 19_2/js/main.js
@@ -22,7 +22,7 @@ function createTodoList(event) {
 }
 form.addEventListener('submit', createTodoList);
 
-input.addEventListener('submit' , function(){
+input.addEventListener('input' , function(){
     if(this.value.length>10){
         this.classList.remove('is-invalid');
         this.classList.add('is-valid');
