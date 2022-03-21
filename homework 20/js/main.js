a4bb579d187c818d80f9b79c1f84b9b3c2b5f56a
snitolya <92542355+snitolya@users.noreply.github.com>
2022-03-21 22:50:49 +0300

    Add files via upload

--- homework 20/js/main.js
+++ homework 20/js/main.js
@@ -7,32 +7,78 @@
 // Изменяйте шаблон при необходимости
 
 
-let ourPage = 1;
-function loadNextUser(page){
-fetch("https://randomuser.me/api/")
+function loadUserInTable(){
+fetch('https://randomuser.me/api')
 .then ((response) => response.json())
 .then((user)=>{
-    console.log(user);
-    const tbody = document.body.querySelector('tbody');
-    console.log(tbody);
+console.log(user);
+const photo = document.body.querySelector('#photo');
+photo.src = user.picture.medium;
+const gender = document.body.querySelector('#gender');
+gender.textContent = user.gender;
+const name = document.body.querySelector('#name');
+name.textContent = `${user.name.first} ${user.name.last}`;
+const email = document.body.querySelector('#email');
+email.textContent = user.email;
+const phone = document.body.querySelector('#phone');
+phone.textContent = user.phone;
+const table = document.body.querySelector('tbody');
+const tableRow = document.createElement('trr');
+tbody.append(tableRow);
 
-})
+});
 }
 
-     loadNextUser(ourPage);
-     const buttonNext = document.body.querySelectorAll('button')[1];
-     console.log(buttonNext);
-     buttonNext.addEventListener('click', ()=>{
-        currentPage++;
-        loadNextUser(ourPage);
-         })
+const buttonAdd = document.body.querySelectorAll('button')[0];
+ console.log(buttonAdd);
+ buttonAdd.addEventListener('click', (e)=>{
+    e.preventDefault();
+    loadUserInTable();
+     })
+
+const buttonNext = document.body.querySelectorAll('button')[1];
+console.log(buttonNext);
+buttonNext.addEventListener('click', (e)=>{
+e.preventDefault();
+e++;
+loadNextUser();
+ })
 
 
 
-// loadUserInTable(currentPage);
-//  const buttonAdd = document.body.querySelectorAll('button')[0];
-//  console.lpg(buttonAdd);
-//  buttonNext.addEventListener('click', ()=>{
-//     currentPage++;
-//     loadUserInTable(currentPage);
-//      })
+ //https://htmlweb.ru/java/example/add_row_in_table.php
+
+
+
+// function loadNextUser(){
+// const buttonNext = document.body.querySelectorAll('button')[1];
+// console.log(buttonNext);
+// buttonNext.addEventListener('click', (e) => {
+//   e.preventDefault();
+//   e++;
+//  })
+//  const photo = document.body.querySelector('#photo');
+//  photo.src = user.picture.medium;
+//  const gender = document.body.querySelector('#gender');
+//  gender.textContent = user.gender;
+//  const name = document.body.querySelector('#name');
+//  name.textContent = `${user.name.first} ${user.name.last}`;
+//  const email = document.body.querySelector('#email');
+//  email.textContent = user.email;
+//  const phone = document.body.querySelector('#phone');
+//  phone.textContent = user.phone;
+//  console.log(phone);
+ 
+// }
+//   fetch('https://randomuser.me/api')
+//     .then((response) => {
+//       console.log(response)
+//       return response.json()
+//     })
+//     .then(user => {
+//       console.log(user)
+//     const userMan = document.querySelector('.col-md-8') 
+//     console.log(userMan);
+    
+//     })
+
