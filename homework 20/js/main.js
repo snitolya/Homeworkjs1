be13789e089ec4b44e478a6e9bbe5efb69e37cbb
snitolya <92542355+snitolya@users.noreply.github.com>
2022-03-22 19:34:17 +0300

    Update main.js

--- homework 20/js/main.js
+++ homework 20/js/main.js
@@ -1,56 +1,45 @@
-// Используя API RandomUser, Реализовать функционал:
-
-// При загрузке страницы грузим юзера
-// Выводим его данные в карточку справа
-// При нажатии кнопки Add добавляем юзера в таблицу (только нужные поля)
-// При нажатии кнопки Next загружаем нового юзера
-// Изменяйте шаблон при необходимости
-
-
 function loadUserInTable(page){
-fetch('https://randomuser.me/api/?page=${page}')
-.then ((response) => response.json())
-.then((user)=>{
-console.log(user);
-const photo = document.body.querySelector('#photo');
-photo.src = user.results[0].picture.medium;
-const gender = document.body.querySelector('#gender');
-gender.textContent = user.results[0].gender;
-const name = document.body.querySelector('#name');
-name.textContent = `${user.results[0].name.first} ${user.results[0].name.last}`;
-const email = document.body.querySelector('#email');
-email.textContent = user.results[0].email;
-const phone = document.body.querySelector('#phone');
-phone.textContent = user.results[0].phone;
- window.user=user.results[0];
- 
-});
+    fetch('https://randomuser.me/api/?page=${page}')
+    .then ((response) => response.json())
+    .then((user)=>{
+    console.log(user);
+    const photo = document.body.querySelector('#photo');
+    photo.src = user.results[0].picture.medium;
+    const gender = document.body.querySelector('#gender');
+    gender.textContent = user.results[0].gender;
+    const name = document.body.querySelector('#name');
+    name.textContent = `${user.results[0].name.first} ${user.results[0].name.last}`;
+    const email = document.body.querySelector('#email');
+    email.textContent = user.results[0].email;
+    const phone = document.body.querySelector('#phone');
+    phone.textContent = user.results[0].phone;
+    window.user=user.results[0];
+     
+    })
 }
-
-
-loadUserInTable(1);
-
-const buttonNext = document.body.querySelectorAll('button')[1].addEventListener('click', function()=>{
-const currentPage = Number.parseInt(this.dataset.page);
-this.dataset.page = currentPage +1;
-loadUserInTable(currentPage+1);
-console.log(this.dataset.page);
-
-
-
-
-const buttonAdd = document.body.querySelectorAll('button')[0].addEventListener('click', function()=>{
-const tr = document.createElement('tr');
-const content = `<td>
-<img src ="${user.picture.medium}" alt="">
-</td>
- <td>${user.gender}</td>
- <td>Açelya Leus</td>
- <td>celya.leus@example.com</td>
- <td>(300)-811-6504</td>`;
-tr.innerHTML = content;
-document.body.querySelector('tbody').append(tr);
-
- 
-
-
+    
+    
+    loadUserInTable(1);
+    
+     document.body.querySelector('.next').addEventListener('click', function(){
+    const currentPage = Number.parseInt(this.dataset.page);
+    this.dataset.page = currentPage +1;
+    loadUserInTable(currentPage+1);
+    console.log(this.dataset.page);
+    })
+    
+    
+    
+    
+    document.body.querySelector('.add').addEventListener('click', function(){
+    const tr = document.createElement('tr');
+    const content = `<td>
+    <img src ="${user.picture.medium}" alt="">
+    </td>
+     <td>${user.gender}</td>
+     <td>Açelya Leus</td>
+     <td>celya.leus@example.com</td>
+     <td>(300)-811-6504</td>`;
+    tr.innerHTML = content;
+    document.body.querySelector('tbody').append(tr);
+    })
