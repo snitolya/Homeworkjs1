6fb1d0ad47987f5e81c63ed1b5793e96a0ea6b28
snitolya <92542355+snitolya@users.noreply.github.com>
2022-03-22 19:05:34 +0300

    Create main.js

--- homework 20/js/main.js
+++ homework 20/js/main.js
@@ -7,27 +7,39 @@
 // Изменяйте шаблон при необходимости
 
 
-function loadUserInTable(){
-fetch('https://randomuser.me/api')
+function loadUserInTable(page){
+fetch('https://randomuser.me/api/?page=${page}')
 .then ((response) => response.json())
-.then((user)=>{
-console.log(user);
+.then((json)=>{
+console.log(json);
 const photo = document.body.querySelector('#photo');
-photo.src = user.picture.medium;
+photo.src = json.results[0].picture.medium;
 const gender = document.body.querySelector('#gender');
-gender.textContent = user.gender;
+gender.textContent = json.results[0].gender;
 const name = document.body.querySelector('#name');
-name.textContent = `${user.name.first} ${user.name.last}`;
+name.textContent = `${json.results[0].name.first} ${json.results[0].name.last}`;
 const email = document.body.querySelector('#email');
-email.textContent = user.email;
+email.textContent = json.results[0].email;
 const phone = document.body.querySelector('#phone');
-phone.textContent = user.phone;
+phone.textContent = json.results[0].phone;
 const table = document.body.querySelector('tbody');
 const tableRow = document.createElement('trr');
 tbody.append(tableRow);
-
+ 
 });
 }
+
+
+loadUserInTable(1);
+
+const buttonNext = document.body.querySelectorAll('button')[1].addEventListener('click', function()=>{
+const currentPage = Number.parseInt(this.dataset.page);
+this.dataset.page = currentPage +1;
+loadUserInTable(currentPage+1);
+console.log(this.dataset.page);
+
+
+
 
 const buttonAdd = document.body.querySelectorAll('button')[0];
  console.log(buttonAdd);
@@ -36,13 +48,6 @@ const buttonAdd = document.body.querySelectorAll('button')[0];
     loadUserInTable();
      })
 
-const buttonNext = document.body.querySelectorAll('button')[1];
-console.log(buttonNext);
-buttonNext.addEventListener('click', (e)=>{
-e.preventDefault();
-e++;
-loadNextUser();
- })
 
 
 
