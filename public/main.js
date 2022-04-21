9e17df8b3b160df21f987c364dcd06f8d20853e5
snitolya <92542355+snitolya@users.noreply.github.com>
2022-04-21 17:26:50 +0300

    Update main.js

--- public/main.js
+++ public/main.js
@@ -87,3 +87,45 @@ class App extends React.Component {
 ReactDOM.render(
 <App />, document.getElementById("root")
 );
+
+
+
+
+
+
+
+
+
+
+
+
+
+class App extends React.Component {
+  constructor(props){
+  super(props);
+  this.state = { images : [
+  'https://cdn.pixabay.com/photo/2019/10/15/13/40/winter-4551699_960_720.jpg',
+  'https://cdn.pixabay.com/photo/2021/04/22/06/40/duck-6198196_960_720.jpg',
+  'https://cdn.pixabay.com/photo/2021/04/17/23/58/bunny-6187026_960_720.jpg',
+  'https://cdn.pixabay.com/photo/2020/09/18/21/16/child-5582985_960_720.jpg'
+  ], activeIndex : 0
+  }
+}
+
+  imageClick = () => {
+    this.setState(state => ({ open: !state.open }))
+   }
+  
+
+  render() {
+    return (
+      <div>
+      <img src={this.state.images[0]} onClick={this.imageClick} />
+    </div>
+    );
+    
+}
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
