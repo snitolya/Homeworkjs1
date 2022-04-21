35c4457d6a6cbf515a626837488f4291531269d6
snitolya <92542355+snitolya@users.noreply.github.com>
2022-04-21 16:51:38 +0300

    Update main.js

--- public/main.js
+++ public/main.js
@@ -43,3 +43,47 @@ const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+class App extends React.Component {
+  constructor(props){
+  super(props);
+  this.state = {open:props.open}
+}
+
+  imageClick = () => {
+    this.setState(state => ({ open: !state.open }))
+   }
+  
+
+  render() {
+    const getImage=  [
+      'https://cdn.pixabay.com/photo/2019/10/15/13/40/winter-4551699_960_720.jpg',
+      'https://cdn.pixabay.com/photo/2021/04/22/06/40/duck-6198196_960_720.jpg',
+      'https://cdn.pixabay.com/photo/2021/04/17/23/58/bunny-6187026_960_720.jpg',
+      'https://cdn.pixabay.com/photo/2020/09/18/21/16/child-5582985_960_720.jpg'
+      ];
+    return (
+  <React.Fragment>
+    {getImage.map(v => <img src={this.state[getImage]} value = {v} onClick={this.imageClick} />)}
+  </React.Fragment>
+     
+    );
+    
+}
+}
+
+
+ReactDOM.render(
+<App />, document.getElementById("root")
+);
